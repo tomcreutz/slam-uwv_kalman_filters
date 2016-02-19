b0f54a79568de37e6caa7e9152f5d6c400559259
Sascha Arnold <sascha.arnold@dfki.de>
2016-02-19 16:30:00 +0100

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace uwv_filters
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace uwv_filters
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <uwv_filters/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    uwv_filters::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
