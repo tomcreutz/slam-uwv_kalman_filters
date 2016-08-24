0fcfad9ff108a5d907cf523aff4ae6b9d1efda4a
Sascha Arnold <sascha.arnold@dfki.de>
2016-08-24 15:29:05 +0200

    use new lite UKF interface

--- src/VelocityUKF.cpp
+++ src/VelocityUKF.cpp
@@ -1,26 +1,7 @@
 #include "VelocityUKF.hpp"
+#include <uwv_dynamic_model/ModelSimulation.hpp>
 
 using namespace uwv_kalman_filters;
-
-const std::string VelocityUKF::acceleration_measurement = "acceleration";
-const std::string VelocityUKF::body_efforts_measurement = "body_efforts";
-const std::string VelocityUKF::angular_velocity_measurement = "angular_velocity";
-
-/**
- * Applies the current acceleration to update the velocity.
- */
-template <typename VelocityState>
-VelocityState
-processAcc(const VelocityState &state, const Eigen::Vector3d& acc, double delta_time)
-{
-    VelocityState new_state(state);
-    new_state.velocity.boxplus(acc, delta_time);
-
-    Eigen::Matrix<double,1,1> z_vel;
-    z_vel(0) = new_state.velocity.z();
-    new_state.z_position.boxplus(z_vel, delta_time);
-    return new_state;
-}
 
 template <typename VelocityState>
 VelocityState
@@ -51,11 +32,25 @@ processMotionModel(const VelocityState &state, boost::shared_ptr<uwv_dynamic_mod
     return new_state;
 }
 
-VelocityUKF::VelocityUKF(const AbstractFilter::FilterState& initial_state)
+template <typename VelocityState>
+VelocityType
+measurementDVL(const VelocityState &state)
 {
-    setInitialState(initial_state);
+    return state.velocity;
+}
 
-    process_noise_cov = MTK_UKF::cov::Zero();
+template <typename VelocityState>
+ZPosType
+measurementPressureSensor(const VelocityState &state)
+{
+    return state.z_position;
+}
+
+VelocityUKF::VelocityUKF(const State& initial_state, const Covariance& state_cov)
+{
+    initializeFilter(initial_state, state_cov);
+
+    process_noise_cov = Covariance::Zero();
     MTK::setDiagonal(process_noise_cov, &WState::velocity, 0.0001);
 }
 
@@ -66,127 +61,69 @@ bool VelocityUKF::setupMotionModel(const uwv_dynamic_model::UWVParameters& param
     prediction_model.reset(new uwv_dynamic_model::ModelSimulation(uwv_dynamic_model::DYNAMIC, 0.01, 1));
     prediction_model->setUWVParameters(parameters);
 
-    FilterState current_state;
+    uwv_dynamic_model::PoseVelocityState model_state;
+    model_state.position = base::Vector3d::Zero();
+    model_state.orientation = base::Quaterniond::Identity();
+    model_state.angular_velocity = angular_velocity.mu;
+    State current_state;
     if(getCurrentState(current_state))
     {
-        uwv_dynamic_model::PoseVelocityState model_state;
-        model_state.position = base::Vector3d::Zero();
-        model_state.orientation = base::Quaterniond::Identity();
-        model_state.linear_velocity = current_state.mu.block(0,0,3,1);
-        model_state.angular_velocity = current_state.mu.block(3,0,3,1);
-        motion_model->setPose(model_state);
+        model_state.linear_velocity = current_state.velocity;
     }
+    motion_model->setPose(model_state);
+
     return true;
 }
 
-void VelocityUKF::predictionStep(const double delta)
+void VelocityUKF::integrateMeasurement(const DVLMeasurement& measurement)
 {
-    MTK_UKF::cov process_noise = process_noise_cov;
+    checkMeasurment(measurement.mu, measurement.cov);
+    ukf->update(measurement.mu, boost::bind(measurementDVL<State>, _1),
+                boost::bind(ukfom::id< DVLMeasurement::Cov >, measurement.cov),
+                ukfom::accept_any_mahalanobis_distance<State::scalar>);
+}
 
-    // use motion model to determine the current acceleration
-    std::map<std::string, pose_estimation::Measurement>::const_iterator body_efforts = latest_measurements.find(body_efforts_measurement);
-    if(body_efforts != latest_measurements.end())
+void VelocityUKF::integrateMeasurement(const GyroMeasurement& measurement)
+{
+    checkMeasurment(measurement.mu, measurement.cov);
+    if(motion_model)
     {
-        if (motion_model.get() == NULL || prediction_model.get() == NULL)
-            throw std::runtime_error("Motion model is not initialized!");
-
-        // apply motion commands
+        // update angular velocity in model
         uwv_dynamic_model::PoseVelocityState model_state = motion_model->getPose();
-        ukf->predict(boost::bind(processMotionModel<WState>, _1, prediction_model, model_state.orientation,
-                                 model_state.angular_velocity, body_efforts->second.mu, delta), MTK_UKF::cov(delta * process_noise));
-
-        // this motion model is updated to have a guess about the current orientation
-        motion_model->setSamplingTime(delta);
-        motion_model->sendEffort(body_efforts->second.mu);
-
-        return;
+        model_state.angular_velocity = measurement.mu;
+        motion_model->setPose(model_state);
     }
-
-    // get acceleration from measurement
-    Eigen::Vector3d acc = Eigen::Vector3d::Zero();
-    std::map<std::string, pose_estimation::Measurement>::const_iterator it = latest_measurements.find(acceleration_measurement);
-    if(it != latest_measurements.end())
-    {
-        acc = it->second.mu;
-        process_noise= it->second.cov;
-    }
-
-    ukf->predict(boost::bind(processAcc<WState>, _1, acc, delta), MTK_UKF::cov(delta * process_noise));
+    angular_velocity = measurement;
 }
 
-bool VelocityUKF::getCurrentState(FilterState& current_state)
+void VelocityUKF::integrateMeasurement(const BodyEffortsMeasurement& measurement)
 {
-    if(ukf.get() != NULL)
-    {
-        UKFStateToMu(ukf->mu(), current_state.mu);
-
-        current_state.cov.resize(WState::DOF + 3, WState::DOF + 3);
-        current_state.cov.setZero();
-        current_state.cov.block(0,0,WState::DOF,WState::DOF) = ukf->sigma();
-
-        // augment covariance with latest angular velocity covariance
-        Eigen::Matrix3d angular_velocity_cov = Eigen::Matrix3d::Zero();
-        std::map<std::string, pose_estimation::Measurement>::const_iterator it = latest_measurements.find(angular_velocity_measurement);
-        if(it != latest_measurements.end())
-        {
-            angular_velocity_cov = it->second.cov;
-        }
-        current_state.cov.block(WState::DOF,WState::DOF,3,3) = angular_velocity_cov;
-        return true;
-    }
-    return false;
+    checkMeasurment(measurement.mu, measurement.cov);
+    body_efforts = measurement;
 }
 
-void VelocityUKF::correctionStepUser(const pose_estimation::Measurement& measurement)
+void VelocityUKF::integrateMeasurement(const PressureMeasurement& measurement)
 {
-    if(measurement.measurement_name == acceleration_measurement)
-    {
-        latest_measurements[measurement.measurement_name] = measurement;
-    }
-    else if(measurement.measurement_name == body_efforts_measurement)
-    {
-        if(motion_model)
-            latest_measurements[measurement.measurement_name] = measurement;
-        else
-            LOG_ERROR_S << "Cannot handle thruster commands, since motion model is not initialized!";
-    }
-    else if(measurement.measurement_name == angular_velocity_measurement)
-    {
-        if(motion_model)
-        {
-            // update angular velocity in model
-            uwv_dynamic_model::PoseVelocityState model_state = motion_model->getPose();
-            model_state.angular_velocity = measurement.mu;
-            motion_model->setPose(model_state);
-        }
-        latest_measurements[measurement.measurement_name] = measurement;
-    }
-    else
-        LOG_ERROR_S << "Measurement " << measurement.measurement_name << " is not supported by the VelocityUKF filter.";
+    checkMeasurment(measurement.mu, measurement.cov);
+    ukf->update(measurement.mu, boost::bind(measurementPressureSensor<State>, _1),
+                boost::bind(ukfom::id< PressureMeasurement::Cov >, measurement.cov),
+                ukfom::accept_any_mahalanobis_distance<State::scalar>);
 }
 
-void VelocityUKF::muToUKFState(const FilterState::Mu& mu, WState& state) const
+void VelocityUKF::predictionStepImpl(double delta)
 {
-    assert(mu.rows() >= WState::DOF);
+    // use motion model to determine the current acceleration
+    if (motion_model.get() == NULL || prediction_model.get() == NULL)
+        throw std::runtime_error("Motion model is not initialized!");
 
-    state.velocity = mu.block(0,0,3,1);
-    state.z_position = mu.block(3,0,1,1);
-}
+    // apply motion commands
+    uwv_dynamic_model::PoseVelocityState model_state = motion_model->getPose();
+    ukf->predict(boost::bind(processMotionModel<WState>, _1, prediction_model, model_state.orientation,
+                                angular_velocity.mu, body_efforts.mu, delta), MTK_UKF::cov(delta * process_noise_cov));
 
-void VelocityUKF::UKFStateToMu(const WState& state, FilterState::Mu& mu) const
-{
-    mu.resize(WState::DOF + 3);
-    mu.setZero();
+    // this motion model is updated to have a guess about the current orientation
+    motion_model->setSamplingTime(delta);
+    motion_model->sendEffort(body_efforts.mu);
 
-    // augment state with latest angular velocity
-    Eigen::Vector3d angular_velocity = Eigen::Vector3d::Zero();
-    std::map<std::string, pose_estimation::Measurement>::const_iterator it = latest_measurements.find(angular_velocity_measurement);
-    if(it != latest_measurements.end())
-    {
-        angular_velocity = it->second.mu;
-    }
-
-    mu.block(0,0,3,1) = state.velocity;
-    mu.block(3,0,1,1) = state.z_position;
-    mu.block(4,0,3,1) = angular_velocity;
+    return;
 }
--- src/VelocityUKF.hpp
+++ src/VelocityUKF.hpp
@@ -1,41 +1,58 @@
 #pragma once
 
+#include <pose_estimation/UnscentedKalmanFilter.hpp>
 #include <pose_estimation/Measurement.hpp>
-#include <pose_estimation/UKF.hpp>
-#include <uwv_dynamic_model/ModelSimulation.hpp>
-#include "VelocityState.hpp"
-#include <map>
+
+#include <uwv_dynamic_model/DataTypes.hpp>
 #include <boost/shared_ptr.hpp>
+#include <mtk/types/SOn.hpp>
+#include <mtk/types/vect.hpp>
+#include <mtk/startIdx.hpp>
+#include <mtk/build_manifold.hpp>
+
+namespace uwv_dynamic_model
+{
+    class ModelSimulation;
+}
 
 namespace uwv_kalman_filters
 {
 
-class VelocityUKF : public pose_estimation::UKF<pose_estimation::VelocityState>
+typedef MTK::vect<3, double> VelocityType;
+typedef MTK::vect<1, double> ZPosType;
+
+MTK_BUILD_MANIFOLD(VelocityState,
+   ((VelocityType, velocity))
+   ((ZPosType, z_position))
+)
+
+class VelocityUKF : public pose_estimation::UnscentedKalmanFilter<VelocityState>
 {
 public:
-    static const std::string acceleration_measurement;
-    static const std::string body_efforts_measurement;
-    static const std::string angular_velocity_measurement;
+    MEASUREMENT(DVLMeasurement, 3)
+    MEASUREMENT(GyroMeasurement, 3)
+    MEASUREMENT(BodyEffortsMeasurement, 6)
+    MEASUREMENT(PressureMeasurement, 1)
 
-    VelocityUKF(const FilterState& initial_state);
+public:
+    VelocityUKF(const State& initial_state, const Covariance& state_cov);
     virtual ~VelocityUKF() {}
 
     bool setupMotionModel(const uwv_dynamic_model::UWVParameters& parameters);
 
-    void predictionStep(const double delta);
-
-    bool getCurrentState(FilterState& current_state);
-
-protected:
-    void correctionStepUser(const pose_estimation::Measurement& measurement);
-
-    void muToUKFState(const FilterState::Mu &mu, WState& state) const;
-    void UKFStateToMu(const WState& state, FilterState::Mu &mu) const;
+    void integrateMeasurement(const DVLMeasurement& measurement);
+    void integrateMeasurement(const GyroMeasurement& measurement);
+    void integrateMeasurement(const BodyEffortsMeasurement& measurement);
+    void integrateMeasurement(const PressureMeasurement& measurement);
 
 protected:
-    std::map<std::string, pose_estimation::Measurement> latest_measurements;
+    void predictionStepImpl(double delta_t);
+
+protected:
     boost::shared_ptr<uwv_dynamic_model::ModelSimulation> motion_model;
     boost::shared_ptr<uwv_dynamic_model::ModelSimulation> prediction_model;
+    GyroMeasurement angular_velocity;
+    BodyEffortsMeasurement body_efforts;
 };
 
 }
